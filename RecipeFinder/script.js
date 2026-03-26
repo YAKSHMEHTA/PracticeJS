bc5ae7a779ab65e8b049f82a02776ed7b5cd34c3
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-03-26 11:42:58 +0530

    meal render

--- RecipeFinder/script.js
+++ RecipeFinder/script.js
@@ -1,21 +1,43 @@
-const API_KEY = "https://www.themealdb.com/api/json/v1/1/search.php?s=cake"
-const hiddenel = document.getElementsByClassName("hidden")
+const API_KEY = "https://www.themealdb.com/api/json/v1/1/search.php?s=cake";
+const hiddenel = document.querySelector(".hidden");
+const inputbox = document.getElementById("search-input");
+const backbtn = document.getElementById("search-btn");
+const meals = document.getElementById("meals");
+const  mealdetails = document.getElementsByClassName("meal-details-content")
+const detailBtn = document.getElementById("detail-button")
 
+function render(data) {
+  meals.innerHTML = data.map((item) => {
+    return `<div class="meal-container">
+  <div class="image-container">
+    <img src=${item.strMealThumb} alt="">
+  </div>
+  <h3>${item.strMeal}</h3>
+  <button id="detail-button" >More Detail</button>
+  </div>`
+  });
+}
 
+meals.addEventListener("click",(e)=>{
+  
+})
 
-function render(data){
+async function getMeals(search) {
+  const res = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`,
+  );
+  if (res === null || res === undefined) {
     hiddenel.style.display = "block";
-    data.forEach(item => {
-        console.log('wasd');
-        console.log(item.strMeal);
-    });
-}
+    return;
+  }
+  const data = await res.json();
 
-
-async function getMeals  (search){
-    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
-    const data = await res.json()
-    render(data.meals)
-    console.log(data);
+  render(data.meals);
+  console.log(data);
 }
-getMeals("cake");
+console.log(inputbox.value);
+backbtn.addEventListener("click", (e) => {
+  console.log("clicked");
+  let search = inputbox.value;
+  getMeals(search);
+});
