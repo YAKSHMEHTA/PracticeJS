eff9bb673546079297bffee685df568d1cac20a5
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-03-27 20:38:23 +0530

    Meal Details

--- RecipeFinder/script.js
+++ RecipeFinder/script.js
@@ -1,5 +1,5 @@
 const API_KEY = "https://www.themealdb.com/api/json/v1/1/search.php?s=cake";
-const hiddenel = document.querySelector(".hidden");
+const hiddenel = document.querySelectorAll(".hidden");
 const inputbox = document.getElementById("search-input");
 const backbtn = document.getElementById("search-btn");
 const meals = document.getElementById("meals");
@@ -13,13 +13,18 @@ function render(data) {
     <img src=${item.strMealThumb} alt="">
   </div>
   <h3>${item.strMeal}</h3>
-  <button id="detail-button" >More Detail</button>
+  <button class="detail-button" data-id=${item.idMeal} >More Detail</button>
   </div>`
-  });
+  }).join("");
 }
 
 meals.addEventListener("click",(e)=>{
-  
+  const button = e.target.closest(".detail-button")
+  if(!button) return 
+  const mealId = button.dataset.id
+  console.log('btn clicked');
+  console.log(mealId);
+  showDetail(mealId)
 })
 
 async function getMeals(search) {
@@ -41,3 +46,24 @@ backbtn.addEventListener("click", (e) => {
   let search = inputbox.value;
   getMeals(search);
 });
+
+async function showDetail(mealId) {
+  mealId = Number(mealId)
+  const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+  const data = await res.json()
+  const obj = data.meals[0]
+  hiddenel[1].style.display = "block"
+  hiddenel[1].innerHTML = `
+    <div class="detail-container">
+      <h2>Recipe-> ${obj.strMeal}</h2>
+      <div class="detai-text">
+        <p>Category-> ${obj.strCategory}</p>
+        <p>Instruction-> ${obj.strInstructions}</p>
+      </div>
+  
+      <div class="detail-img">
+        <img src=${obj.strMealThumb} alt="">
+        </div>
+    </div>
+  `
+}
